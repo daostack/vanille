4e3b65b892d3224ab8d73ba8f8674930e4a5c0c6
Doug Kent <github@douglaskent.com>
2018-01-11 02:41:44 -0800

    tweaks and bug fixes

--- src/schemeDashboards/SchemeRegistrar.ts
+++ src/schemeDashboards/SchemeRegistrar.ts
@@ -1,10 +1,10 @@
 import { autoinject, computedFrom, observable } from "aurelia-framework";
 import { DaoSchemeDashboard } from "./schemeDashboard"
-import { SchemeService, SchemeInfo } from  "../services/SchemeService";
+import { SchemeService, SchemeInfo } from "../services/SchemeService";
 import { OrganizationService } from '../services/OrganizationService';
-import { ArcService, ContractInfo, SchemeRegistrar, ProposeToAddModifySchemeParams, ProposeToRemoveSchemeParams } from  "../services/ArcService";
-import { EventAggregator  } from 'aurelia-event-aggregator';
-import { SchemeConfigurator} from '../schemeConfiguration/schemeConfigurationBase';
+import { ArcService, ContractInfo, SchemeRegistrar, ProposeToAddModifySchemeParams, ProposeToRemoveSchemeParams } from "../services/ArcService";
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { SchemeConfigurator } from '../schemeConfiguration/schemeConfigurationBase';
 import { EventConfigTransaction, EventConfigException } from "../entities/GeneralEvents";
 import { NonArcSchemeItemName } from "../resources/customElements/arcSchemesDropdown/arcSchemesDropdown";
 
@@ -12,10 +12,10 @@ import { NonArcSchemeItemName } from "../resources/customElements/arcSchemesDrop
 export class SchemeRegistrarDashboard extends DaoSchemeDashboard {
 
   modifiedSchemeConfiguration: SchemeConfigurator = <any>{};
-  schemeToModify: SchemeInfo=null;
-  schemeToRemove: SchemeInfo=null;
-  newSchemeConfiguration: SchemeConfigurator = <any>{ canBeRegisteringScheme : true };
-  @observable currentSchemeSelection: SchemeInfo=null;
+  schemeToModify: SchemeInfo = null;
+  schemeToRemove: SchemeInfo = null;
+  newSchemeConfiguration: SchemeConfigurator = <any>{ canBeRegisteringScheme: true };
+  @observable currentSchemeSelection: SchemeInfo = null;
   schemeToAddAddress: string;
   hasSchemesToAdd: boolean;
   addableSchemes: Array<SchemeInfo> = [];
@@ -36,17 +36,16 @@ export class SchemeRegistrarDashboard extends DaoSchemeDashboard {
   }
 
   currentSchemeSelectionChanged() {
-      if (this.currentSchemeSelection)
-      {
-        this.schemeToAddAddress = this.currentSchemeSelection.address;
-      } else {
-        this.schemeToAddAddress = undefined;
-      }
-      if (this.schemeToAddAddress) {
-        $(this.addressControl).addClass("is-filled"); // annoying thing you have to do for BMD
-      } else {
-        $(this.addressControl).removeClass("is-filled"); // annoying thing you have to do for BMD
-      }
+    if (this.currentSchemeSelection) {
+      this.schemeToAddAddress = this.currentSchemeSelection.address;
+    } else {
+      this.schemeToAddAddress = undefined;
+    }
+    if (this.schemeToAddAddress) {
+      $(this.addressControl).addClass("is-filled"); // annoying thing you have to do for BMD
+    } else {
+      $(this.addressControl).removeClass("is-filled"); // annoying thing you have to do for BMD
+    }
   }
 
   @computedFrom("currentSchemeSelection")
@@ -55,31 +54,34 @@ export class SchemeRegistrarDashboard extends DaoSchemeDashboard {
   }
 
   async addScheme() {
-    try{
-      
+    try {
+
       const schemeRegistrar = await this.arcService.getContract("SchemeRegistrar") as SchemeRegistrar;
       let config: ProposeToAddModifySchemeParams = Object.assign({
-          avatar: this.orgAddress
-          , scheme: this.schemeToAddAddress
-          , schemeParametersHash: await this.newSchemeConfiguration.getConfigurationHash(this.orgAddress, this.schemeToAddAddress)
-        }, this.newSchemeConfiguration, this.addSchemeFees /*, {autoRegister: this. autoRegister }*/);
+        avatar: this.orgAddress
+        , scheme: this.schemeToAddAddress
+        , schemeParametersHash: await this.newSchemeConfiguration.getConfigurationHash(this.orgAddress, this.schemeToAddAddress)
+      }, this.newSchemeConfiguration, this.addSchemeFees /*, {autoRegister: this. autoRegister }*/);
 
 
       if (!this.isNonArcScheme) {
         config.schemeName = this.currentSchemeSelection.name;
       }
-      
+
       let tx = await schemeRegistrar.proposeToAddModifyScheme(config);
 
       this.eventAggregator.publish("handleSuccess", new EventConfigTransaction(
         `Proposal submitted to add ${this.schemeToAddAddress}`, tx.tx));
-    } catch(ex) {
-        this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing to add scheme ${this.schemeToAddAddress}`, ex));
-    }  
+
+      this.currentSchemeSelection = null;
+
+    } catch (ex) {
+      this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing to add scheme ${this.schemeToAddAddress}`, ex));
+    }
   }
 
   async modifyScheme() {
-  
+
     try {
       const schemeRegistrar = await this.arcService.getContract("SchemeRegistrar") as SchemeRegistrar;
       const schemeParametersHash = await this.modifiedSchemeConfiguration.getConfigurationHash(this.orgAddress, this.schemeToModify.address);
@@ -94,8 +96,8 @@ export class SchemeRegistrarDashboard extends DaoSchemeDashboard {
       this.eventAggregator.publish("handleSuccess", new EventConfigTransaction(
         `Proposal submitted to modify ${this.schemeToModify.address}`, tx.tx));
 
-    } catch(ex) {
-        this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing to modify scheme ${this.schemeToModify.address}`, ex));
+    } catch (ex) {
+      this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing to modify scheme ${this.schemeToModify.address}`, ex));
     }
   }
 
@@ -105,15 +107,18 @@ export class SchemeRegistrarDashboard extends DaoSchemeDashboard {
 
       const schemeRegistrar = await this.arcService.getContract("SchemeRegistrar") as SchemeRegistrar;
 
-      let tx = await schemeRegistrar.proposeToRemoveScheme( {
+      let tx = await schemeRegistrar.proposeToRemoveScheme({
         avatar: this.orgAddress,
         scheme: this.schemeToRemove.address
       });
 
       this.eventAggregator.publish("handleSuccess", new EventConfigTransaction(
         `Proposal submitted to remove ${this.schemeToRemove.address}`, tx.tx));
-    } catch(ex) {
-        this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing to remove scheme ${this.schemeToRemove.address}`, ex));
+
+      this.schemeToRemove = null;
+
+    } catch (ex) {
+      this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing to remove scheme ${this.schemeToRemove.address}`, ex));
     }
   }
 }
--- src/schemeDashboards/UpgradeScheme.ts
+++ src/schemeDashboards/UpgradeScheme.ts
@@ -1,7 +1,7 @@
 import { autoinject, computedFrom, observable } from 'aurelia-framework';
 import { DaoSchemeDashboard } from "./schemeDashboard"
-import { EventAggregator  } from 'aurelia-event-aggregator';
-import { ArcService } from  "../services/ArcService";
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { ArcService } from "../services/ArcService";
 import { SchemeService, SchemeInfo } from '../services/SchemeService';
 import { EventConfigTransaction, EventConfigException } from "../entities/GeneralEvents";
 import { NonArcSchemeItemName } from "../resources/customElements/arcSchemesDropdown/arcSchemesDropdown";
@@ -10,8 +10,8 @@ import { NonArcSchemeItemName } from "../resources/customElements/arcSchemesDrop
 export class UpgradeScheme extends DaoSchemeDashboard {
 
   controllerAddress: string;
-  upgradingSchemeConfig:any = {};
-  @observable currentSchemeSelection: SchemeInfo=null;
+  upgradingSchemeConfig: any = {};
+  @observable currentSchemeSelection: SchemeInfo = null;
   upgradingSchemeAddress: string;
   NonArcSchemeItemKey = NonArcSchemeItemName;
   schemesExclusiveOfUpgradeScheme: Array<string>;
@@ -19,12 +19,12 @@ export class UpgradeScheme extends DaoSchemeDashboard {
   schemeFees: any = { fee: 0, tokenAddress: undefined }
 
   constructor(
-      private schemeService: SchemeService
+    private schemeService: SchemeService
     , private eventAggregator: EventAggregator
     , private arcService: ArcService
   ) {
     super();
-    this.schemesExclusiveOfUpgradeScheme =  arcService.arcSchemes
+    this.schemesExclusiveOfUpgradeScheme = arcService.arcSchemes
       .map((s) => s.name)
       .filter((name) => name !== "UpgradeScheme");
   }
@@ -35,17 +35,16 @@ export class UpgradeScheme extends DaoSchemeDashboard {
   }
 
   currentSchemeSelectionChanged() {
-      if (this.currentSchemeSelection)
-      {
-        this.upgradingSchemeAddress = this.currentSchemeSelection.address;
-      } else {
-        this.upgradingSchemeAddress = undefined;
-      }
-      if (this.upgradingSchemeAddress) {
-        $(this.addressControl).addClass("is-filled"); // annoying thing you have to do for BMD
-      } else {
-        $(this.addressControl).removeClass("is-filled"); // annoying thing you have to do for BMD
-      }
+    if (this.currentSchemeSelection) {
+      this.upgradingSchemeAddress = this.currentSchemeSelection.address;
+    } else {
+      this.upgradingSchemeAddress = undefined;
+    }
+    if (this.upgradingSchemeAddress) {
+      $(this.addressControl).addClass("is-filled"); // annoying thing you have to do for BMD
+    } else {
+      $(this.addressControl).removeClass("is-filled"); // annoying thing you have to do for BMD
+    }
   }
 
   async proposeController() {
@@ -60,20 +59,20 @@ export class UpgradeScheme extends DaoSchemeDashboard {
       this.eventAggregator.publish("handleSuccess", new EventConfigTransaction(
         'Proposal submitted to change controller ${this.controllerAddress}', tx.tx));
 
-    } catch(ex) {
-        this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing new controller ${this.controllerAddress}`, ex));
+    } catch (ex) {
+      this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing new controller ${this.controllerAddress}`, ex));
     }
   }
 
   async submitUpgradingScheme() {
     try {
       const scheme = await this.arcService.getContract("UpgradeScheme");
-      let config:any = {
-          avatar: this.orgAddress
-        };
+      let config: any = {
+        avatar: this.orgAddress
+      };
 
       config.scheme = this.upgradingSchemeAddress;
-      
+
       config.schemeParametersHash = await this.upgradingSchemeConfig.getConfigurationHash(
         this.orgAddress, scheme.address);
 
@@ -81,8 +80,11 @@ export class UpgradeScheme extends DaoSchemeDashboard {
 
       this.eventAggregator.publish("handleSuccess", new EventConfigTransaction(
         'Proposal submitted to change upgrading scheme ${this.upgradingSchemeAddress}', tx.tx));
-    } catch(ex) {
-        this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing new upgrade scheme ${this.upgradingSchemeAddress}`, ex));
+
+      this.currentSchemeSelection = null;
+
+    } catch (ex) {
+      this.eventAggregator.publish("handleException", new EventConfigException(`Error proposing new upgrade scheme ${this.upgradingSchemeAddress}`, ex));
     }
   }
 }
