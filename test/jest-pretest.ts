e9d881b92a06d711b008f32361d9aeec2f2d54b2
Doug Kent <github@douglaskent.com>
2018-01-23 13:08:58 -0500

    cleaned up rebase errors

--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -3,6 +3,12 @@ import { Options } from 'aurelia-loader-nodejs';
 import { globalize } from 'aurelia-pal-nodejs';
 import * as path from 'path';
 Options.relativeToDir = path.join(__dirname, 'unit');
-// NOTE: we're manually including jsdom@v11.1.0 to workaround: https://github.com/aurelia/pal-nodejs/issues/24
+/**
+ * NOTE: we're using a strict set of test package versions because they
+ * appear to be easily broken by newer versions. 
+ * Here is one example: https://github.com/aurelia/pal-nodejs/issues/24
+ * So if we ever want to upgrade test-related packages, it is recommended
+ * to do so carefully, one-at-a-time, rerunning the tests as you go.
+ */
 globalize();
 process.env.network = "test";
