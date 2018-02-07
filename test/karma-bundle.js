92276165d37052473d9973167c1132a3d8313a66
Doug Kent <github@douglaskent.com>
2018-02-07 14:48:55 -0500

    fixed unit tests (I hope)

--- test/karma-bundle.js
+++ test/karma-bundle.js
@@ -1,6 +1,6 @@
-import "aurelia-polyfills";
-import "aurelia-loader-webpack";
-import {install as installJasmineAsync} from "jest-jasmine2/jasmine-async";
+import 'aurelia-polyfills';
+import 'aurelia-loader-webpack';
+import { install as installJasmineAsync } from 'jest-jasmine2/jasmine-async';
 
 // enable running Promise-returning tests:
 installJasmineAsync(global);
@@ -19,7 +19,7 @@ runTests(testModuleContexts);
 function loadTestModules() {
   const srcContext = require.context(
     // directory:
-    "../src",
+    '../src',
     // recursive:
     true,
     // tests in /src folder regex:
@@ -28,7 +28,7 @@ function loadTestModules() {
 
   const testContext = require.context(
     // directory:
-    "./unit",
+    './unit',
     // recursive:
     true,
     // tests in ./unit folder regex:
@@ -51,7 +51,7 @@ function requireAllInContext(requireContext) {
  * See https://github.com/facebook/jest/blob/master/packages/jest-jasmine2/src/jest-expect.js
  */
 function installJestExpect() {
-  const expect = require("jest-matchers");
+  const expect = require('jest-matchers');
   global.expect = expect;
 
   const jasmine = global.jasmine;
@@ -63,13 +63,13 @@ function installJestExpect() {
   jasmine.addMatchers = (jasmineMatchersObject) => {
     const jestMatchersObject = Object.create(null);
     Object.keys(jasmineMatchersObject).forEach(name => {
-      jestMatchersObject[name] = function() {
+      jestMatchersObject[name] = function () {
         const result = jasmineMatchersObject[name](jasmine.matchersUtil, null);
         // if there is no 'negativeCompare', both should be handled by `compare`
         const negativeCompare = result.negativeCompare || result.compare;
 
         return this.isNot
-          ? negativeCompare(...arguments)
+          ? negativeCompare.apply(null, arguments)
           : result.compare.apply(null, arguments);
       };
     });
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -8,12 +8,12 @@ exports.config = {
   // use `npm start -- e2e`
 
   specs: [
-    "**/*.e2e.ts"
+    '**/*.e2e.ts'
   ],
 
   exclude: [],
 
-  framework: "jasmine",
+  framework: 'jasmine',
 
   allScriptsTimeout: 110000,
 
@@ -30,18 +30,18 @@ exports.config = {
   directConnect: true,
 
   capabilities: {
-    "browserName": "chrome",
-    "chromeOptions": {
-      "args": [
-        "--show-fps-counter",
-        "--no-default-browser-check",
-        "--no-first-run",
-        "--disable-default-apps",
-        "--disable-popup-blocking",
-        "--disable-translate",
-        "--disable-background-timer-throttling",
-        "--disable-renderer-backgrounding",
-        "--disable-device-discovery-notifications",
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': [
+        '--show-fps-counter',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--disable-default-apps',
+        '--disable-popup-blocking',
+        '--disable-translate',
+        '--disable-background-timer-throttling',
+        '--disable-renderer-backgrounding',
+        '--disable-device-discovery-notifications',
         /* enable these if you'd like to test using Chrome Headless
           '--no-gpu',
           '--headless'
@@ -51,10 +51,10 @@ exports.config = {
   },
 
   onPrepare: function() {
-    require("ts-node").register({ compilerOptions: { module: "commonjs" }, disableWarnings: true, fast: true });
+    require('ts-node').register({ compilerOptions: { module: 'commonjs' }, disableWarnings: true, fast: true });
   },
 
   plugins: [{
-    package: "aurelia-protractor-plugin"
+    package: 'aurelia-protractor-plugin'
   }],
 };
